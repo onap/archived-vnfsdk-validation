5c98348e1588a42aad799cefd5693ecdece188b3
AvinashS <avinash.s@huawei.com>
2017-09-07 13:46:54 +0000

    VNF Validation seed code
    Skeleton structure
    Reorganised code with tool as a jar
    for csar validation as an initial step.
    
    Change-Id: Ie5895c885872fb186f2acd8c8bb0f823210e906f
    IssueId: VNFSDK-76
    Signed-off-by: AvinashS <avinash.s@huawei.com>

---
+++ csarvalidation/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.onap.vnfsdk.validation.validation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
