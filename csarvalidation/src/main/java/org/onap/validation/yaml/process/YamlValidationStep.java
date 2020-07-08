992dab6e7e67c82a0c916fdf34bc798076da1440
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2020-07-08 14:37:27 +0200

    Add validation of YAML documents, with use of schema
    Issue-ID: VNFSDK-594
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: I4afc573f564c642a349736e50b50c591b9b18650

---
+++ csarvalidation/src/main/java/org/onap/validation/yaml/process/YamlValidationStep.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Nokia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.onap.validation.yaml.process;
+
+import org.onap.validation.yaml.model.YamlDocument;
+import org.onap.validation.yaml.schema.node.YamlSchemaNode;
+
+import java.util.List;
+
+class YamlValidationStep {
+
+    private final List<YamlSchemaNode> schemaNodes;
+    private final YamlDocument document;
+
+    YamlValidationStep(List<YamlSchemaNode> nodes, YamlDocument yaml) {
+        this.schemaNodes = nodes;
+        this.document = yaml;
+    }
+
+    List<YamlSchemaNode> getSchemaNodes() {
+        return schemaNodes;
+    }
+
+    YamlDocument getDocument() {
+        return document;
+    }
+
+}
