a69ba4a0b0a132c2a419db5b8b750334b3d3f302
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2020-10-07 11:30:01 +0200

    Refactor functional tests for CSAR validation.
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: Icaa53313d66a01dccc779959b49b74857402cb71
    Issue-ID: VNFSDK-661

--- csarvalidation/src/test/java/org/onap/cvc/csar/CsarValidatorTest.java
+++ csarvalidation/src/test/java/org/onap/cvc/csar/CsarValidatorTest.java
@@ -17,19 +17,19 @@
 
 package org.onap.cvc.csar;
 
-import org.apache.commons.lang3.StringUtils;
-import org.assertj.core.api.Assertions;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.onap.cli.fw.cmd.OnapCommand;
 import org.onap.cli.fw.error.OnapCommandException;
 import org.onap.cli.fw.output.OnapCommandResult;
-import org.onap.cli.fw.output.OnapCommandResultAttribute;
 import org.onap.cli.main.OnapCli;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.onap.cvc.csar.cc.sol004.IntegrationTestUtils.absoluteFilePath;
 
 
@@ -42,19 +42,30 @@ public class CsarValidatorTest {
     @Test
     public void shouldReportThanVnfValidationFailed() throws URISyntaxException {
         // given
-        OnapCliWrapper cli = new OnapCliWrapper(new String[]{
-            "--product", "onap-dublin",
-            "csar-validate",
-            "--format", "json",
-            "--csar", absoluteFilePath("VoLTE.csar")});
+        OnapCliWrapper cli = new OnapCliWrapper(createExecutionRequestData(false, "VoLTE.csar"));
 
         // when
         cli.handle();
 
         // then
-        final OnapCommandResult onapCommandResult = cli.getCommandResult();
-        verifyThatOperation(onapCommandResult, OPERATION_STATUS_FAILED);
-        verifyThatXRulesFails(onapCommandResult, 7);
+        final CliValidationResponseWrapper result = getCliCommandValidationResult(cli);
+        assertThat(result.criteria).isEqualTo(OPERATION_STATUS_FAILED);
+        result.results.forEach((ruleValidationResult)->{
+            if ( ruleHaveOneOfCodes(ruleValidationResult.vnfreqName,
+                "r01123", "r09467")
+            ) {
+                assertThat(ruleValidationResult.errors)
+                    .hasSize(2);
+            } else if ( ruleHaveOneOfCodes(ruleValidationResult.vnfreqName,
+                "r21322","r26885","r43958" ,"r66070","r130206")
+            ) {
+                assertThat(ruleValidationResult.errors)
+                    .hasSize(1);
+            } else {
+                assertThat(ruleValidationResult.errors).hasSize(0);
+            }
+        });
+
         verifyThatOperationFinishedWithoutAnyError(cli);
     }
 
@@ -62,101 +73,128 @@ public class CsarValidatorTest {
     @Test
     public void shouldReportOnlyWarningWhenCsarDoNotHaveCertificateAndHashesInManifest() throws URISyntaxException {
         // given
-        OnapCliWrapper cli = new OnapCliWrapper(new String[]{
-            "--product", "onap-dublin",
-            "csar-validate",
-            "--format", "json",
-            "--pnf",
-            "--csar", absoluteFilePath("pnf/validFile.csar")});
+        CliValidationResponseWrapper.ValidationResultWrapper.ValidationErrorWrapper expectedWarning =
+            createExpectedError(CERTIFICATION_RULE, "0x1006",
+                "Warning. Consider adding package integrity and authenticity assurance according to ETSI NFV-SOL 004 Security Option 1");
+
+        OnapCliWrapper cli = new OnapCliWrapper(createExecutionRequestData(true, "pnf/validFile.csar"));
+
         // when
         cli.handle();
 
         // then
-        final OnapCommandResult onapCommandResult = cli.getCommandResult();
-        verifyThatOperation(onapCommandResult, OPERATION_STATUS_PASS);
-        assertTrue(onapCommandResult.getOutput().toString().contains(
-            "\"warnings\":[{" +
-                "\"vnfreqNo\":\"R130206\"," +
-                "\"code\":\"0x1006\"," +
-                "\"message\":\"Warning. Consider adding package integrity and authenticity assurance according to ETSI NFV-SOL 004 Security Option 1\"," +
-                "\"file\":\"\"," +
-                "\"lineNumber\":-1}]"));
+        final CliValidationResponseWrapper result = getCliCommandValidationResult(cli);
+
+        assertThat(result.criteria).isEqualTo(OPERATION_STATUS_PASS);
+        result.results.forEach((ruleValidationResult)->{
+            assertThat(ruleValidationResult.errors).hasSize(0);
+            if (ruleValidationResult.vnfreqName.equals(CERTIFICATION_RULE)) {
+                assertThat(ruleValidationResult.warnings)
+                    .hasSize(1)
+                    .containsOnly(expectedWarning);
+            } else {
+                assertThat(ruleValidationResult.warnings).hasSize(0);
+            }
+        });
         verifyThatOperationFinishedWithoutAnyError(cli);
+    }
+
+    private CliValidationResponseWrapper.ValidationResultWrapper.ValidationErrorWrapper createExpectedError(
+        String rule, String errorCode, String errorMessage
+    ) {
+        return new CliValidationResponseWrapper.ValidationResultWrapper.ValidationErrorWrapper(
+            rule.toUpperCase(), errorCode,
+            errorMessage,
+            "", "-1"
+        );
     }
 
     @Test
     public void shouldNotReportThatPnfValidationFailedWhenZipDoNotHaveCertificatesAndHashesInManifest() throws URISyntaxException {
         // given
-        OnapCliWrapper cli = new OnapCliWrapper(new String[]{
-            "--product", "onap-dublin",
-            "csar-validate",
-            "--format", "json",
-            "--pnf",
-            "--csar", absoluteFilePath("pnf/signed-package-valid-signature.zip")});
+        OnapCliWrapper cli = new OnapCliWrapper(createExecutionRequestData(true, "pnf/signed-package-valid-signature.zip"));
 
         // when
         cli.handle();
 
         // then
-        final OnapCommandResult onapCommandResult = cli.getCommandResult();
-        verifyThatOperation(onapCommandResult, OPERATION_STATUS_PASS);
+        final CliValidationResponseWrapper result = getCliCommandValidationResult(cli);
+
+        assertThat(result.criteria).isEqualTo(OPERATION_STATUS_PASS);
         verifyThatOperationFinishedWithoutAnyError(cli);
     }
 
     @Test
     public void shouldReportThatPnfValidationFailedWhenCsarContainsCertificateInCmsAndInToscaAndInRootAndHashIsIncorrect_allOtherRulesShouldPass() throws URISyntaxException {
         // given
-        OnapCliWrapper cli = new OnapCliWrapper(new String[]{
-            "--product", "onap-dublin",
-            "csar-validate",
-            "--format", "json",
-            "--pnf",
-            "--csar", absoluteFilePath("pnf/r130206/cert-in-cms-and-root-and-tosca-incorrect-hash.csar")});
+
+        List<CliValidationResponseWrapper.ValidationResultWrapper.ValidationErrorWrapper> expectedErrors =
+            List.of(
+                createExpectedError(CERTIFICATION_RULE, "0x4007",
+                    "File has invalid signature!"),
+                createExpectedError(CERTIFICATION_RULE, "0x4004",
+                    "Source 'Files/pnf-sw-information/pnf-sw-information.yaml' has wrong hash!"),
+                createExpectedError(CERTIFICATION_RULE, "0x4011",
+                    "ETSI-Entry-Certificate entry in Tosca.meta is defined despite the certificate is included in the signature container"),
+                createExpectedError(CERTIFICATION_RULE, "0x4012",
+                    "ETSI-Entry-Certificate certificate present despite the certificate is included in the signature container"),
+                createExpectedError(CERTIFICATION_RULE, "0x4013",
+                    "Certificate present in root catalog despite the certificate is included in the signature container")
+            );
+        OnapCliWrapper cli = new OnapCliWrapper(createExecutionRequestData(true, "pnf/r130206/cert-in-cms-and-root-and-tosca-incorrect-hash.csar"));
+
         // when
         cli.handle();
 
         // then
-        final OnapCommandResult onapCommandResult = cli.getCommandResult();
-        verifyThatOperation(onapCommandResult, OPERATION_STATUS_FAILED);
-        verifyThatXRulesFails(onapCommandResult, 1);
-        verifyThatRuleFails(onapCommandResult, CERTIFICATION_RULE);
+        final CliValidationResponseWrapper result = getCliCommandValidationResult(cli);
+
+        assertThat(result.criteria).isEqualTo(OPERATION_STATUS_FAILED);
+        result.results.forEach((ruleValidationResult)->{
+            assertThat(ruleValidationResult.warnings).hasSize(0);
+            if (ruleValidationResult.vnfreqName.equals(CERTIFICATION_RULE)) {
+                assertThat(ruleValidationResult.errors)
+                    .hasSize(5)
+                    .containsAll(expectedErrors);
+            } else {
+                assertThat(ruleValidationResult.errors).hasSize(0);
+            }
+        });
         verifyThatOperationFinishedWithoutAnyError(cli);
     }
 
-    private void verifyThatXRulesFails(OnapCommandResult onapCommandResult, int noOfRules) {
-        verifyDataOccurrenceInResult(onapCommandResult, "\"passed\":false", noOfRules);
+    private String[] createExecutionRequestData(boolean isPnf, String csarPath) throws URISyntaxException {
+        String[] executionParameters;
+        if (isPnf) {
+            executionParameters = new String[]{
+                "--product", "onap-dublin",
+                "csar-validate",
+                "--format", "json",
+                "--pnf",
+                "--csar", absoluteFilePath(csarPath)
+            };
+        } else {
+            executionParameters = new String[]{
+                "--product", "onap-dublin",
+                "csar-validate",
+                "--format", "json",
+                "--csar", absoluteFilePath(csarPath)
+            };
+        }
+        return executionParameters;
     }
 
-    private void verifyThatRuleFails(OnapCommandResult onapCommandResult, String nameOfRule) {
-        final String value = String.format("\"passed\":false,\"vnfreqName\":\"%s\"", nameOfRule);
-        verifyDataOccurrenceInResult(onapCommandResult, value, 1);
+    private CliValidationResponseWrapper getCliCommandValidationResult(OnapCliWrapper cli) {
+        final OnapCommandResult onapCommandResult = cli.getCommandResult();
+        return new Gson().fromJson(onapCommandResult.getOutput().toString(), CliValidationResponseWrapper.class);
     }
 
-    private void verifyDataOccurrenceInResult(OnapCommandResult onapCommandResult, String data, int dataOccurrence) {
-        final String json = getResultValue(onapCommandResult, "results");
-        int occurrence = StringUtils.countMatches(json, data);
-        Assertions.assertThat(occurrence).isEqualTo(dataOccurrence);
-    }
-
-    private void verifyThatOperation(OnapCommandResult onapCommandResult, String operationStatus) {
-        final String value = getResultValue(onapCommandResult, "criteria");
-        Assertions.assertThat(value).isEqualTo(operationStatus);
-    }
-
-    private String getResultValue(OnapCommandResult onapCommandResult, String attributeName) {
-        final OnapCommandResultAttribute results = getOnapCommandResultAttribute(onapCommandResult, attributeName);
-        return results.getValues().get(0);
-    }
-
-    private OnapCommandResultAttribute getOnapCommandResultAttribute(OnapCommandResult onapCommandResult, String name) {
-        return onapCommandResult.getRecords().stream()
-                .filter(it -> it.getName().equals(name))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException(String.format("Unable to find '%s' attribute!", name)));
+    private boolean ruleHaveOneOfCodes(String ruleCode, String... codes ) {
+        return Arrays.asList(codes).contains(ruleCode);
     }
 
     private void verifyThatOperationFinishedWithoutAnyError(OnapCliWrapper cli) {
-        assertEquals(0, cli.getExitCode());
+        assertThat(cli.getExitCode()).isEqualTo(0);
     }
 
     static class OnapCliWrapper extends OnapCli {
@@ -176,5 +214,62 @@ public class CsarValidatorTest {
         OnapCommandResult getCommandResult() {
             return this.commandResult;
         }
+    }
+
+
+    static class CliValidationResponseWrapper {
+
+        VnfDataWrapper vnf;
+        String data;
+        String criteria;
+        List<ValidationResultWrapper> results;
+        String contact;
+        String platform;
+
+        static class VnfDataWrapper {
+            String name;
+            String vendor;
+            String version;
+            String type;
+            String mode;
+        }
+
+        static class ValidationResultWrapper {
+            Boolean passed;
+            String vnfreqName;
+            String description;
+            List<ValidationErrorWrapper> errors;
+            List<ValidationErrorWrapper> warnings;
+
+            static class ValidationErrorWrapper {
+                String vnfreqNo;
+                String code;
+                String message;
+                String file;
+                String lineNumber;
+
+                public ValidationErrorWrapper(String vnfreqNo, String code, String message, String file, String lineNumber) {
+                    this.vnfreqNo = vnfreqNo;
+                    this.code = code;
+                    this.message = message;
+                    this.file = file;
+                    this.lineNumber = lineNumber;
+                }
+
+                @Override
+                public boolean equals(Object o) {
+                    if (this == o) return true;
+                    if (o == null || getClass() != o.getClass()) return false;
+                    return o.hashCode() == this.hashCode();
+                }
+
+                @Override
+                public int hashCode() {
+                    return Objects.hash(vnfreqNo, code, message, file, lineNumber);
+                }
+            }
+
+        }
+
     }
 }
