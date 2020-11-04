3bf8320fad2b57c3d6faa3006d3904459a5a1f09
Kanagaraj Manickam <kanagaraj.manickam@huawei.com>
2020-11-04 10:27:37 +0530

    Add support for CNF-conformance check
    Issue-ID: VNFSDK-592
    
    Signed-off-by: Kanagaraj Manickam <kanagaraj.manickam@huawei.com>
    Change-Id: I0641f3a52e5e82eec6ff47a9911879efb72cc4d7

---
+++ cnf-conformance/README.md
@@ -0,0 +1,7 @@
+Integratation of CNF coformacne tool with VTP:
+
+To enable this integration, 
+1. Copy the run.sh under VTP OCOMP bin dir
+2. copy the cnf-conformance-check.yaml under VTP OCOMP open-cli-home dir
+3. Run OCOMP schema-refresh command to enable this test case in VTP
+
---
+++ cnf-conformance/run.sh
@@ -0,0 +1,23 @@
+#    Copyright (C) 2020, Huawei Technologies, Ltd.
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#            http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+#Helps to run cnf-conformance test cases.
+#Follow the guidelines detailed in https://github.com/cncf/cnf-conformance
+
+# Arg-1: CNF dir where the cnf-conformance.yml is placed
+	
+cd $1
+
+cnf-conformance setup > setup.txt
+cnf-conformance all cnf-config=./cnf-conformance.yml > all.txt
+
+cat setup.txt
+cat all.txt
