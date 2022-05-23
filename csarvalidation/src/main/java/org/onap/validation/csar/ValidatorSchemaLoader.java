26f72bfa3ab9c40788bf028d95a4fa5f8d434ed3
prakash.e@huawei.com <prakash.e@huawei.com>
2022-05-23 20:13:17 +0530

    Removed unused import statements. .
    https://sonarcloud.io/project/issues?resolved=false&severities=MINOR&id=onap_vnfsdk-validation&open=AXnx7xQ5aKaQcuB-KHhk
    https://sonarcloud.io/project/issues?resolved=false&severities=MINOR&id=onap_vnfsdk-validation&open=AXQFW52-3TFchjqB3cM9
    
    Issue-ID: VNFSDK-832
    
    Signed-off-by: prakash.e@huawei.com <prakash.e@huawei.com>
    Change-Id: Ib0d9ec18aa9158661cea4004b9cd4440833c3f91

--- csarvalidation/src/main/java/org/onap/validation/csar/ValidatorSchemaLoader.java
+++ csarvalidation/src/main/java/org/onap/validation/csar/ValidatorSchemaLoader.java
@@ -18,8 +18,7 @@ package org.onap.validation.csar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.*;
-import java.net.URISyntaxException;
+
 import java.util.*;
 import org.yaml.snakeyaml.Yaml;
 
